822e8fead7fd874d4e674949ae68abb2031e2bdb
Marc Greenfield <mgreenf1@stevens.edu>
2017-11-28 20:26:13 -0500

    small edits

--- main_script.py
+++ main_script.py
@@ -4,28 +4,25 @@ import time
 import smtplib
 import sys
 
-while True:
-    url = "https://www.jcrew.com/p/mens_category/shoes/sneakers/nike-for-jcrew-killshot-2-sneakers/85231"
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-    response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.text, "lxml")
 
-    if str(soup).find("add to bag") == -1:
-        time.sleep(10800)
-        continue
+url = "https://www.jcrew.com/p/mens_category/shoes/sneakers/nike-for-jcrew-killshot-2-sneakers/85231"
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+response = requests.get(url, headers=headers)
+soup = BeautifulSoup(response.text, "lxml")
 
-    else:
-        password = sys.argv[2]
-        msg = "Check JCrew for Killshots!"
-        fromaddr = sys.argv[1]
-        toaddr = sys.argv[1]
+if str(soup).find("add to bag") == -1:
+    sys.exit()
 
-        server = smtplib.SMTP('smtp.gmail.com', 587)
-        server.ehlo()
-        server.starttls()
-        server.login(fromaddr, password)
+else:
+    password = sys.argv[2]
+    msg = "Check JCrew for Killshots!"
+    fromaddr = sys.argv[1]
+    toaddr = sys.argv[1]
 
-        server.sendmail(fromaddr, toaddr, msg)
-        server.quit()
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login(fromaddr, password)
 
-        break
+    server.sendmail(fromaddr, toaddr, msg)
+    server.quit()
